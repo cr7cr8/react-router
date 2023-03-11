d43264a0633dbf7f9bf4111dd95cf2ef4f2adfda
cr7cr8 <cr7cr8@126.com>
2023-03-11 13:15:24 +0800

    6666

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   RouterProvider
 } from 'react-router-dom'
 
-// pages //4444444444444
+// pages //6666
 import Home from './pages/Home'
 import About from './pages/About'
 import Faq from './pages/help/Faq'
